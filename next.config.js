3284475ff3906f4f13f3d5fe1a0124ccfa5b8702
kafuikumah <kafui.kofi.kumah@gmail.com>
2024-01-01 22:39:39 +0000

    Project set up

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+const { withContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "tailwindui.com",
+      "images.unsplash.com",
+      "cdn.dribbble.com",
+      "m.media-amazon.com",
+      "ik.imagekit.io",
+      "miro.medium.com",
+      "store.storeimages.cdn-apple.com",
+      "www.apple.com",
+      "i.ytimg.com",
+    ],
+  },
+};
+
+module.exports = withContentlayer(nextConfig);
